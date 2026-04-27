efa791e32603e1c1ccc206397678c99e2514ce44
25020308-DangGiaPhong <25020308@vnu.edu.vn>
2026-04-27 21:01:39 +0700

    Add files via upload

--- Bai5/src/test/java/CalculatorTest.java
+++ Bai5/src/test/java/CalculatorTest.java
@@ -1,11 +1,21 @@
 import org.junit.jupiter.api.Test;
-import static
-org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
+    CalculatorService service = new CalculatorService();
     @Test
     public void testAdd() {
-        CalculatorService service = new CalculatorService();
+
         assertEquals(5, service.solve(2, 3, "add"));
+    }
+    @Test
+    public void testSub() {
+
+        assertEquals(5, service.solve(7, 2, "sub"));
+    }
+    @Test
+    public void testMul() {
+
+        assertEquals(6, service3.solve(2, 3, "mul"));
     }
 }
